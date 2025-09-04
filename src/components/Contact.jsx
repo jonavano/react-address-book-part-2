a4f3c8f8c2c81c88f37037ba8ef24d4ff9555ad8
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-09-04 09:40:24 +0000

    allow deleting

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,25 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ContactContext } from "../App";
 
 function Contact({contact, id}) {
+        const {url, setContacts}  = useContext(ContactContext)
+    
+
+    const deleteUser = (e) => {
+        e.preventDefault()
+        console.log("delete user")
+
+        const deleteData = async () => {
+            await fetch(url+'/' + contact.id, { method:'DELETE' });};
+        deleteData();
+        setContacts(prev => prev.filter((c) => c.id !== contact.id))
+    }
 
     return <>
     <p>{contact.firstName + " " + contact.lastName}</p>
     <Link to={`/profile/${id}`}><p>View</p></Link>
+    <button onClick={deleteUser}>Delete</button>
     </>
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,33 @@
 import { useContext } from "react";
 import { ContactContext } from "../App";
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 
 function Profile() {
-    const { contacts } = useContext(ContactContext)
+    const { contacts, url, setContacts } = useContext(ContactContext)
     const { id } = useParams();
+      const navigate = useNavigate();
 
     const contact = contacts[id];
+    
+
+    const deleteUser = (e) => {
+        e.preventDefault()
+        console.log("delete user")
+
+        const deleteData = async () => {
+            await fetch(url+'/' + contact.id, { method:'DELETE' });};
+        deleteData();
+        setContacts(prev => prev.filter((c) => c.id !== contact.id))
+        navigate('/')
+
+    }
 
     return <>
         <h2>{contact.firstName + " " + contact.lastName} </h2>
         <p>{contact.street + " " + contact.city}</p>
+            <button onClick={deleteUser}>Delete</button>
+
     </>
 }
 
